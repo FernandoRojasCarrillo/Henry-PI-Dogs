9488424c9aed785e8f1813124950564337dbf357
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2022-06-13 11:33:09 -0500

    one more

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -17,10 +17,13 @@ export default function Home(){
   const dispatch = useDispatch();
   let  ShowDogs = useSelector(state => state.ShowDogs);
   let  Current = useSelector(state => state.Current);
+  let  AllDogs = useSelector(state => state.AllDogs);
   
   useEffect( ()=>{
     dispatch(getAllDogs())
   },[])
+
+  const value = Math.ceil(AllDogs.length / 8)
 
 
   return (
@@ -43,30 +46,31 @@ export default function Home(){
                 <div className='paginado' >
                   <button onClick={() => dispatch(MoveBachward())} className='btn-buttons' >Back</button>
                   <div className='container-paginado ' >
-                    <button className={Current === 1 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(1))} >1</button>
-                    <button className={Current === 2 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(2))} >2</button>
-                    <button className={Current === 3 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(3))} >3</button>
-                    <button className={Current === 4 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(4))} >4</button>
-                    <button className={Current === 5 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(5))} >5</button>
-                    <button className={Current === 6 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(6))} >6</button>
-                    <button className={Current === 7 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(7))} >7</button>
-                    <button className={Current === 8 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(8))} >8</button>
-                    <button className={Current === 9 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(9))} >9</button>
-                    <button className={Current === 10 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(10))} >10</button>
-                    <button className={Current === 11 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(11))} >11</button>
-                    <button className={Current === 12 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(12))} >12</button>
-                    <button className={Current === 13 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(13))} >13</button>
-                    <button className={Current === 14 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(14))} >14</button>
-                    <button className={Current === 15 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(15))} >15</button>
-                    <button className={Current === 16 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(16))} >16</button>
-                    <button className={Current === 17 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(17))} >17</button>
-                    <button className={Current === 18 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(18))} >18</button>
-                    <button className={Current === 19 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(19))} >19</button>
-                    <button className={Current === 20 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(20))} >20</button>
-                    <button className={Current === 21 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(21))} >21</button>
-                    <button className={Current === 22 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(22))} >22</button>
+                    
+                    <button className={ value + 1 <= 1 ? 'block' :  Current === 1 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(1))} >1</button>
+                    <button className={ value + 1 <= 2 ? 'block' :  Current === 2 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(2))} >2</button>
+                    <button className={ value + 1 <= 3 ? 'block' :  Current === 3 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(3))} >3</button>
+                    <button className={ value + 1 <= 4 ? 'block' :  Current === 4 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(4))} >4</button>
+                    <button className={ value + 1 <= 5 ? 'block' :  Current === 5 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(5))} >5</button>
+                    <button className={ value + 1 <= 6 ? 'block' :  Current === 6 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(6))} >6</button>
+                    <button className={ value + 1 <= 7 ? 'block' :  Current === 7 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(7))} >7</button>
+                    <button className={ value + 1 <= 8 ? 'block' :  Current === 8 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(8))} >8</button>
+                    <button className={ value + 1 <= 9 ? 'block' :  Current === 9 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(9))} >9</button>
+                    <button className={ value + 1 <= 10 ? 'block' : Current === 10 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(10))} >10</button>
+                    <button className={ value + 1 <= 11 ? 'block' : Current === 11 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(11))} >11</button>
+                    <button className={ value + 1 <= 12 ? 'block' : Current === 12 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(12))} >12</button>
+                    <button className={ value + 1 <= 13 ? 'block' : Current === 13 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(13))} >13</button>
+                    <button className={ value + 1 <= 14 ? 'block' : Current === 14 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(14))} >14</button>
+                    <button className={ value + 1 <= 15 ? 'block' : Current === 15 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(15))} >15</button>
+                    <button className={ value + 1 <= 16 ? 'block' : Current === 16 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(16))} >16</button>
+                    <button className={ value + 1 <= 17 ? 'block' : Current === 17 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(17))} >17</button>
+                    <button className={ value + 1 <= 18 ? 'block' : Current === 18 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(18))} >18</button>
+                    <button className={ value + 1 <= 19 ? 'block' : Current === 19 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(19))} >19</button>
+                    <button className={ value + 1 <= 20 ? 'block' : Current === 20 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(20))} >20</button>
+                    <button className={ value + 1 <= 21 ? 'block' : Current === 21 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(21))} >21</button>
+                    <button className={ value + 1 <= 22 ? 'block' : Current === 22 ? 'btns btn-active' : 'btns'} onClick={() => dispatch(GetAndShowAllDogs(22))} >22</button>
                   </div>
-                  {/* <button onClick={() => dispatch(MoveForward())}  className='btn-buttons' >Ahead</button> */}
+                  <button onClick={() => dispatch(MoveForward())}  className='btn-buttons' >Ahead</button>
                 </div>
               ): (<div></div>)
             }
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -10,6 +10,7 @@ from '../actions';
 const inisialState = {
   Current: 1,
   AllDogs: [],
+  AuxDogs: [],
   ShowDogs: [],
   getDogDetail: {}
 }
@@ -17,91 +18,42 @@ const inisialState = {
 export default function Reducer(state=inisialState, action) {
   switch (action.type) {
     case GET_ALL_DOGS:
+      let Dogs = [...action.payload];
+      let Show = [];
+      while(Dogs.length){
+        let ArrayOfDogs = [];
+        let c = 8;
+        while(c > 0 && Dogs.length > 0) {
+          c --
+          ArrayOfDogs.push(Dogs.shift());
+        }
+        Show.push(ArrayOfDogs);
+      }
       return{
         ...state,
         AllDogs: action.payload,
-        ShowDogs: action.payload.slice(0,8)
+        AuxDogs: [...Show],
+        ShowDogs: action.payload.slice(0,8) || action.payload.slice(0,action.payload.length)
       }
     case GET_AND_SHOW_ALL_DOGS:
       return {
         ...state,
-        Current: action.payload,
-        ShowDogs: action.payload === 1 ? state.AllDogs.slice(0,8) :  
-        action.payload === 2 ? state.AllDogs.slice(8,16) :
-        action.payload === 3 ? state.AllDogs.slice(16,24) :
-        action.payload === 4 ? state.AllDogs.slice(24,32) :
-        action.payload === 5 ? state.AllDogs.slice(32,40) :
-        action.payload === 6 ? state.AllDogs.slice(40,48) :
-        action.payload === 7 ? state.AllDogs.slice(48,56) :
-        action.payload === 8 ? state.AllDogs.slice(56,64) :
-        action.payload === 9 ? state.AllDogs.slice(64,72) :
-        action.payload === 10 ? state.AllDogs.slice(72,80) :
-        action.payload === 11 ? state.AllDogs.slice(80,88) :
-        action.payload === 12 ? state.AllDogs.slice(88,96) :
-        action.payload === 13 ? state.AllDogs.slice(96,104) :
-        action.payload === 14 ? state.AllDogs.slice(104,112) :
-        action.payload === 15 ? state.AllDogs.slice(112,120) :
-        action.payload === 16 ? state.AllDogs.slice(120,128) :
-        action.payload === 17 ? state.AllDogs.slice(128,136) :
-        action.payload === 18 ? state.AllDogs.slice(136,144) :
-        action.payload === 19 ? state.AllDogs.slice(144,152) :
-        action.payload === 20 ? state.AllDogs.slice(152,160) :
-        action.payload === 21 ? state.AllDogs.slice(160,168) :
-        action.payload === 22 ? state.AllDogs.slice(168, state.AllDogs.length) : []
+        ShowDogs: [...state.AuxDogs[action.payload - 1]] ,
+        Current: action.payload
       }
     case MOVE_FORWARD:
       return{
         ...state,
-        ShowDogs: state.Current === 1 ? state.AllDogs.slice(8,16) :  
-        state.Current === 2 ? state.AllDogs.slice(16,24) :
-        state.Current === 3 ? state.AllDogs.slice(24,32) :
-        state.Current === 4 ? state.AllDogs.slice(32,40) :
-        state.Current === 5 ? state.AllDogs.slice(40,48) :
-        state.Current === 6 ? state.AllDogs.slice(48,56) :
-        state.Current === 7 ? state.AllDogs.slice(56,64) :
-        state.Current === 8 ? state.AllDogs.slice(64,72) :
-        state.Current === 9 ? state.AllDogs.slice(72,80) :
-        state.Current === 10 ? state.AllDogs.slice(80,88) :
-        state.Current === 11 ? state.AllDogs.slice(88,96) :
-        state.Current === 12 ? state.AllDogs.slice(96,104) :
-        state.Current === 13 ? state.AllDogs.slice(104,112) :
-        state.Current === 14 ? state.AllDogs.slice(112,120) :
-        state.Current === 15 ? state.AllDogs.slice(120,128) :
-        state.Current === 16 ? state.AllDogs.slice(128,136) :
-        state.Current === 17 ? state.AllDogs.slice(136,144) :
-        state.Current === 18 ? state.AllDogs.slice(144,152) :
-        state.Current === 19 ? state.AllDogs.slice(152,160) :
-        state.Current === 20 ? state.AllDogs.slice(160,168) :
-        state.Current === 21 ? state.AllDogs.slice(168, state.AllDogs.length) :
-        state.Current === 22 ? state.AllDogs.slice(0,8) : [],
-        Current: state.Current === 22 ? 1 : state.Current + 1
+        ShowDogs: state.Current === Math.ceil(state.AllDogs.length/8) ? [...state.AuxDogs[0]] : [...state.AuxDogs[state.Current]] ,
+        Current: state.Current === Math.ceil(state.AllDogs.length/8) ? 1 : state.Current + 1
       }
+        
     case MOVE_BACKWARD:
+      // let End = Math.ceil(state.AllDogs.length / 8)
       return{
         ...state,
-        ShowDogs: state.Current === 1 ? state.AllDogs.slice(168, state.AllDogs.length) :  
-        state.Current === 2 ? state.AllDogs.slice(0,8) :
-        state.Current === 3 ? state.AllDogs.slice(8,16) :
-        state.Current === 4 ? state.AllDogs.slice(16,24) :
-        state.Current === 5 ? state.AllDogs.slice(24,32) :
-        state.Current === 6 ? state.AllDogs.slice(32,40) :
-        state.Current === 7 ? state.AllDogs.slice(40,48) :
-        state.Current === 8 ? state.AllDogs.slice(48,56) :
-        state.Current === 9 ? state.AllDogs.slice(56,64) :
-        state.Current === 10 ? state.AllDogs.slice(64,72) :
-        state.Current === 11 ? state.AllDogs.slice(72,80) :
-        state.Current === 12 ? state.AllDogs.slice(80,88) :
-        state.Current === 13 ? state.AllDogs.slice(88,96) :
-        state.Current === 14 ? state.AllDogs.slice(96,104) :
-        state.Current === 15 ? state.AllDogs.slice(104,112) :
-        state.Current === 16 ? state.AllDogs.slice(112,120) :
-        state.Current === 17 ? state.AllDogs.slice(120,128) :
-        state.Current === 18 ? state.AllDogs.slice(128,136) :
-        state.Current === 19 ? state.AllDogs.slice(136,144) :
-        state.Current === 20 ? state.AllDogs.slice(144,152) :
-        state.Current === 21 ? state.AllDogs.slice(152,160) :
-        state.Current === 22 ? state.AllDogs.slice(160,168) : [],
-        Current: state.Current === 1 ? 22 : state.Current - 1
+        ShowDogs: state.Current === 1 ? [...state.AuxDogs[Math.ceil(state.AllDogs.length/8 - 1)]] : [...state.AuxDogs[state.Current - 2]],
+        Current: state.Current === 1 ? Math.ceil(state.AllDogs.length/8)  : state.Current - 1
       }
     default:
       return state;
