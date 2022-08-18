1014e2bb9c00f421ddd8b22d2d555c8b0264d5d8
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2022-08-18 11:55:10 -0500

    commit prueba

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
-const app  = require('./src/app.js');
-const PORT = 3000;
-const { db } = require('./src/db.js');
+// const app  = require('./src/app.js');
+// const { db } = require('./src/db.js');
+const express = require('express');
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-  db.sync({ force: false });
+app.get('/', (req,res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3000}`);
+  // db.sync({ force: false });
 });
